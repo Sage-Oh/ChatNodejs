7ad8523fa02d1a178a6e320806437de258f48183
Sage-Oh <7838qq@gmail.com>
2022-04-30 15:42:02 +0900

    update

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
 const app = express();
-const path = require("path");
+//node.js 의 기본 모듈 path 불러오기
+//path 는 url을 쉽게 만들어주는 도구
+const path = require('path');
 const server = http.createServer(app);
-const socketIO = require("socket.io");
+
+const socketIO = require('socket.io');
+
+//socketIO에서 서버를 담아가는 내용을 변수로 담음, 그리고 이 io 를 통해서 받은 메시지 등을 제어할 것임
 const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, "src")))
-
-const PORT = process.env.PORT || 5000 ;
-
-
-io.on("connection",(socket)=>{
-  console.log('연결이 이뤄졌다')
-})
+//시간을 사용하기 위해 모먼트 변수 사용
+const moment = require("moment");
 
 
-server.listen(PORT, ()=>console.log(`server is running ${PORT}`))
+console.log(__dirname);
+//path.join 을 사용하는 이유는 운영체제마다 경로(url)을 나타내는 /, \ 가 다르기 때문
+app.use(express.static(path.join(__dirname, 'src')))
+//서버를 실행하기 위해서는 port 가 필요.
+//process 환경에 port가 지정이 되어 있으면 그것을 사용하고 아니라면 5000번 사용
+const PORT = process.env.PORT || 5000;
+
+// connection 이벤트가 발생하면 이 모든것을 socket에 담는다
+// 데이터를 받는 부분
+io.on("connection", (socket)=>{
+  //서버에서 채팅을 받아주는 코드 작성: "채팅아이디", 콜백함수
+  socket.on("chatting", (data)=>{
+    //이 data는 프론트에서 넘겨받은 data
+    const { name, msg } = data;
+
+    io.emit("chatting", {
+      //두 개는 이름이 같아서 생략이 가능하다
+      name: name,
+      msg: msg,
+      time: moment().format("HH:mm")
+    })
+  });
+});
+
+
+
+// 서버 실행. (포트, 명령=콜백함수) , port 라는 변수를 사용하기 위해서 ₩사용 템플릿 문법으로
+server.listen(PORT, ()=>console.log(`server is running ${PORT}`));
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,1 +1,76 @@
-console.log("hello.js");
+//use strict 를 통해서 자바스크립트의 오류를 최대한 줄인다
+"use strict"
+
+const socket = io();
+
+//우리가 사용할 dom 선택하기
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+
+//엔터치면 전송되는 거
+chatInput.addEventListener("keypress", (event)=>{
+  //키코드값이 13 = 엔터
+  if(event.keyCode === 13){
+    send();
+    
+  }
+  //헐 해결했다
+  
+});
+
+function send(){
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value
+  };
+  chatInput.value="";
+  //채팅을 강제로 보낸다 emit메서드 사용 (채널이름=채널아이디, 보낼 내용)
+  //이렇게 하면 서버(app.js)에서 받을 수 있다.
+  socket.emit("chatting", param);
+}
+
+//버튼을 눌렀을 때 실행되어야 하기때문에 button에 이벤트를 걸어준다
+sendButton.addEventListener("click", send);
+
+
+// 서버에서 보낸 내용이 여기에 담김
+socket.on("chatting", (data)=>{
+  // //소켓을 받았을 때  에 내용을 넣어주고 받기
+  // const li = document.createElement("li");
+  // //템플릿 문법
+  // li.innerText = `${data.name}님이 - ${data.msg}`;
+
+  // //이것을 input 해주기 - 
+  // chatList.appendChild(li);
+
+  //li모델을 인스턴스화 시켜주기
+  const {name, msg, time} = data;
+  const item = new LiModel(name, msg, time);
+  item.makeLi()
+  //얘의 스크롤 값을 읽어서 밑으로 작동되도록 하는 것
+  displayContainer.scrollTo(0, displayContainer.scrollHeight)
+
+});
+
+function LiModel(name, msg, time){
+  this.name = name;
+  this.msg = msg;
+  this.time = time;
+
+  this.makeLi = ()=>{
+    const li = document.createElement("li");
+    li.classList.add(nickname.value=== this.name ? "sent" : "received")
+    const dom = `<span class="profile">
+    <span class="user">${this.name}</span>
+    <img src="https://item.kakaocdn.net/do/a1866850b14ae47d0a2fd61f409dfc057154249a3890514a43687a85e6b6cc82" alt="any">
+  </span>
+  <span class="message">${this.msg}</span>
+  <span class="time">${this.time}</span>`  ;
+  li.innerHTML = dom;
+  chatList.appendChild(li);
+
+  };
+};
